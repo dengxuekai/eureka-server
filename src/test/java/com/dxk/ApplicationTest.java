e4789479dba0fbdd936a73c6492b380e427af483
邓学凯 <hzdengxuekai@corp.netease.com>
2018-09-29 10:28:42 +0800

    修改配置文件

---
+++ src/test/java/com/dxk/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dxk;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by hzdengxuekai on 2018/6/26.
+ */
+public class ApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
